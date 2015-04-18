14b5fb5c94893bfe1fe2aa21acbb7dd2e9acf293
Sam Symons <sam@samsymons.com>
2015-04-18 15:55:18 -0700

    Implement a few more basic instructions.

--- Emulate/Emulators/8080/EightyEightyCPU.swift
+++ Emulate/Emulators/8080/EightyEightyCPU.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 struct EightyEightyConditionCodes {
-    var z: UInt8 = 0 // Zero flag, for when a result is 0
-    var s: UInt8 = 0 // Sign flag, set when the MSB is 1
-    var p: UInt8 = 0 // Parity flag
-    var cy: UInt8 = 0
-    var ac: UInt8 = 0
+    var z: Bool = false // Zero flag, for when a result is 0
+    var s: Bool = false // Sign flag, set when the MSB is 1
+    var p: Bool = false // Parity flag
+    var cy: Bool = false // Carry flag
+    var ac: Bool = false // Auxillary carry
 }
 
 struct EightyEightyState {
@@ -45,21 +45,29 @@ class EightyEightyCPU: NSObject {
         var executionLength = 0
         
         // while self.state.pc < 10 {
-        while executionLength < 10 {
+        while executionLength < 30 {
             let byte = bytes[Int(self.state.pc)]
             var length = 1
             
             switch byte {
             case 0x00, 0x10, 0x20, 0x30:
                 length = nop(byte)
+            case 0x04:
+                self.state.b += 1
+                logicFlagsZSP(self.state.b)
+            case 0x05:
+                self.state.b -= 1
+                logicFlagsZSP(self.state.b)
             case 0x06:
                 length = mviBd8(byte)
+            // case 0x13:
+            // length = inxD(byte)
+            case 0x21:
+                length = lxi(byte)
             case 0xc3:
                 length = jmp(byte)
             case 0xcd:
-                length = jmp(byte)
-            case 0xe6:
-                length = jmp(byte)
+                length = calla16(byte)
             default:
                 length = unimplementedInstruction(byte)
             }
@@ -70,6 +78,11 @@ class EightyEightyCPU: NSObject {
     }
     
     // MARK: - Private
+    
+    private func resetCPU() {
+        self.conditionCodes = EightyEightyConditionCodes()
+        self.state = EightyEightyState()
+    }
     
     private func dataFromPath(path: String) -> UnsafeBufferPointer<UInt8> {
         let data = NSData(contentsOfFile: path)
@@ -83,27 +96,47 @@ class EightyEightyCPU: NSObject {
         return UnsafePointer<UInt16>(bytes).memory
     }
     
+    private func byte(offset: Int) -> UInt8 {
+        return self.data![Int(self.state.pc + UInt16(offset))]
+    }
+    
     // MARK: - Instructions
     
-    func nop(byte: UInt8) -> Int {
-        println(NSString(format: "NOP: %02hhx", byte))
+    func nop(opcode: UInt8) -> Int {
+        println(NSString(format: "NOP: %02hhx", opcode))
         return 1
     }
     
-    func mviBd8(byte: UInt8) -> Int {
-        println(NSString(format: "MVI B, d8: %02hhx", byte))
+    func decrB(opcode: UInt8) -> Int {
+        // s.d, s.e = hilo(adr(s.d, s.e) + 1)
         
-        let argument = self.data![Int(self.state.pc + UInt16(1))]
-        self.state.b = argument
+        return 1;
+    }
+    
+    func inxD(opcode: UInt8) -> Int {
+        // s.d, s.e = hilo(adr(s.d, s.e) + 1)
+        
+        return 1;
+    }
+    
+    func lxi(opcode: UInt8) -> Int {
+        println(NSString(format: "LXI H, d16: %02hhx", opcode))
+        
+        self.state.h = byte(2)
+        self.state.l = byte(1)
+        
+        return 3
+    }
+    
+    func mviBd8(opcode: UInt8) -> Int {
+        println(NSString(format: "MVI B, d8: %02hhx", opcode))
+        self.state.b = byte(1)
         
         return 2
     }
     
-    func jmp(byte: UInt8) -> Int {
-        let hiByte = self.data![Int(self.state.pc + UInt16(1))]
-        let loByte = self.data![Int(self.state.pc + UInt16(2))]
-        
-        let address = memoryAddress(hiByte, lo: loByte)
+    func jmp(opcode: UInt8) -> Int {
+        let address = memoryAddress(byte(1), lo: byte(2))
         self.state.pc = address
         
         println(NSString(format: "JUMPING: %04lx", address))
@@ -111,8 +144,58 @@ class EightyEightyCPU: NSObject {
         return 3
     }
     
-    func unimplementedInstruction(byte: UInt8) -> Int {
-        println(NSString(format: "Unimplemented instruction: %02hhx", byte))
+    func unimplementedInstruction(opcode: UInt8) -> Int {
+        println(NSString(format: "Unimplemented instruction: %02hhx", opcode))
         return 1
+    }
+    
+    // MARK: - Calling
+    
+    func call(address: UInt16) {
+        println("CALL: UNIMPLEMENTED")
+    }
+    
+    func calla16(opcode: UInt8) -> Int {
+        let address = memoryAddress(byte(1), lo: byte(2))
+        call(address)
+        
+        return 3
+    }
+    
+    // MARK: - Arithmetic
+    
+    func ana(byte: UInt8) {
+        self.state.a &= byte
+        logicFlagsA()
+    }
+    
+    func xra(byte: UInt8) {
+        self.state.a ^= byte
+        logicFlagsA()
+    }
+    
+    func ora(byte: UInt8) {
+        self.state.a |= byte
+        logicFlagsA()
+    }
+    
+    // MARK: - Flags
+    
+    func logicFlagsA() {
+        self.conditionCodes.cy = false
+        self.conditionCodes.ac = false
+        self.conditionCodes.z = (self.state.a == UInt8(0))
+        self.conditionCodes.s = (0x80 == self.state.a & 0x80)
+        self.conditionCodes.p = parity(self.state.a)
+    }
+    
+    func logicFlagsZSP(value: UInt8) {
+        self.conditionCodes.z = value == 0
+        self.conditionCodes.s = 0x80 == (value & 0x80)
+        self.conditionCodes.p = parity(value)
+    }
+    
+    func parity(byte: UInt8) -> Bool {
+        return false
     }
 }
